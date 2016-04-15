7b97aedb7452211e81021f70d2952d39e1db3823
liwang <81391276@qq.com>
2016-04-15 23:20:56 +0800

    编译通过调整jar加载顺序，文件化冲突的java类，指定配置xml文件对应得xsd文件，加了一点注释

---
+++ spring-core-4.0/org/springframework/core/io/package-info.java
@@ -0,0 +1,8 @@
+
+/**
+ *（工具类均声明为abstract）
+ * Generic abstraction for (file-based) resources, used throughout the framework.
+ *
+ */
+package org.springframework.core.io;
+
---
+++ src/test/java/com/thinkgem/jeesite/liwang/Main/Try.java
@@ -0,0 +1,28 @@
+package com.thinkgem.jeesite.liwang.Main;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+
+import org.springframework.util.StringUtils;
+
+public class Try {
+	
+	public static void main(String[] args) {
+		
+		for(String s:StringUtils.delimitedListToStringArray("http://nishui/kkj..//sa", "/"))
+			System.out.println(s);;
+	}
+	
+	public static String subTime(String firstTime,String lastTime){
+		return subTime(firstTime,lastTime,"hhmm");
+	}
+	public static String subTime(String firstTime,String lastTime,String reg){
+		SimpleDateFormat sdf=new SimpleDateFormat(reg);
+		try {
+			long dif=sdf.parse(lastTime).getTime()-sdf.parse(firstTime).getTime();
+			return dif/1000/60/60+"个小时"+(dif/1000/60-(dif/1000/60/60)*60)+"分钟";
+		} catch (ParseException e) {
+			return "";
+		}
+	}
+}
